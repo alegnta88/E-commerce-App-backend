f9be1507d2bad00740b3fc51a38f54f7d928ebfe
root <root@alegnta.digaf.local>
2025-10-15 14:07:10 +0300

    index-change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const itemRoutes = require("./routes/itemRoutes");
 const userRoutes = require("./routes/userRoute");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
